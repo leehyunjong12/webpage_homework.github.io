ce7a73da69b907b38ee89ae278d035c6988aa8c3
leehyunjong12 <alclssha4665@gmail.com>
2024-05-31 12:48:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function btnClick(){
+    const intro=
+    document.getElementById('intro');
+        intro.style.display ='block';
+        const one=
+    document.getElementById('one');
+        one.style.display ='none';
+        const two=
+    document.getElementById('two');
+        two.style.display ='none';
+}
+function btn2Click(){
+    const intro=
+    document.getElementById('intro');
+        intro.style.display ='none';
+        const one=
+    document.getElementById('one');
+        one.style.display ='block';
+        const two=
+    document.getElementById('two');
+        two.style.display ='none';
+}
+function btn3Click(){
+    const intro=
+    document.getElementById('intro');
+        intro.style.display ='none';
+        const one=
+    document.getElementById('one');
+        one.style.display ='none';
+        const two=
+    document.getElementById('two');
+        two.style.display ='block';
+}
+
